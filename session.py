4f7cd5b2df34f7e08051fbf152d2cad1762a9e00
abee0502 <104993291+abee0502@users.noreply.github.com>
2025-06-01 12:59:43 +0300

    Create session.py

---
+++ session.py
@@ -0,0 +1,11 @@
+from utils.loader import load_progress, save_progress
+
+def get_today_batch(flashcards, day):
+    start = (day - 1) * 40
+    end = day * 40
+    return flashcards[start:end]
+
+def increment_day():
+    progress = load_progress()
+    progress["day"] = min(progress["day"] + 1, 7)
+    save_progress(progress)

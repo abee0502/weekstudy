be7c6f176fe1b448f25a426d07699b549e0d1bf7
abee0502 <104993291+abee0502@users.noreply.github.com>
2025-06-01 12:59:55 +0300

    Create scoring.py

---
+++ scoring.py
@@ -0,0 +1,18 @@
+from utils.loader import load_progress, save_progress
+
+def save_answer(qid, correct):
+    progress = load_progress()
+    progress["answered"][str(qid)] = "correct" if correct else "wrong"
+    save_progress(progress)
+
+def get_progress():
+    progress = load_progress()
+    answered_today = sum(1 for qid in progress["answered"] if int(qid) <= progress["day"] * 40 and (int(qid) > (progress["day"] - 1) * 40))
+    return {
+        "answered_today": answered_today,
+        "total_answered": len(progress["answered"]),
+        "day": progress["day"]
+    }
+
+def reset_progress():
+    save_progress({"day": 1, "answered": {}})
